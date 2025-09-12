8240141ac5cad95f029ff9d87382e5b8833a4060
edot <adiwikrama27@gmail.com>
2025-09-12 21:18:47 +0800

    create app and system settings

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import customtkinter
 from pytubefix import YouTube
 
+# Systems Settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# Create App
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("YouTube Video Downloader")
