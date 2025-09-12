6f92cde96f25c7c4d700503f385ded24b993f263
edot <adiwikrama27@gmail.com>
2025-09-12 21:21:55 +0800

    create download function

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 import customtkinter
 from pytubefix import YouTube
 
+# Download Function + Error/Success UI Text
+def startDownload():
+    global finishLabel
+    try:
+        yt = YouTube(str(link.get()))
+        stream = yt.streams.get_highest_resolution()
+        title.configure(text=yt.title, font=customtkinter.CTkFont(size=15, weight="bold"), text_color="white")
+        if 'finishLabel' in globals() and finishLabel.winfo_exists():
+            finishLabel.destroy()
+        finishLabel = customtkinter.CTkLabel(app, text="Download Completed!", font=customtkinter.CTkFont(size=20, weight="bold"), text_color="green")
+        finishLabel.pack(padx=10, pady=10)
+        stream.download()
+    except Exception:
+        if 'finishLabel' in globals() and finishLabel.winfo_exists():
+            finishLabel.destroy()
+        finishLabel = customtkinter.CTkLabel(app, text="An error occurred. Please check the URL and try again.", font=customtkinter.CTkFont(size=20, weight="bold"), text_color="red")
+        finishLabel.pack(padx=10, pady=10)
+    finally:
+        link.delete(0, 'end')
+
+
 # Systems Settings
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
